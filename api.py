fe2bdb77b87588a5cb35527b4785c768a6903d1d
100hnomeunome <tales.775@gmail.com>
2024-10-05 15:33:04 -0300

    hello

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import requests
 # refactored at some point.
 def get_product_from_api(product_id):
     try:
-        r = requests.get(f"https://api.provider.ext/products/{product_id}")
+        r = requests.get(f"https://api.provider.ext/products/{product_id}", timeout=5)
         return r.json()
     except Exception:
         print("something bad happened")
